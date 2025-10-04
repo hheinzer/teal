ff9149c68ca556366a33aac166da1c867480e3fe
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-10-04 22:06:48 +0200

    Protect builtin functions if they are not present (TCC)

--- src/teal/assert.h
+++ src/teal/assert.h
@@ -2,8 +2,30 @@
 
 #define STATIC_ASSERT(expr) extern int STATIC_ASSERT[(expr) ? 1 : -1]
 
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+#if __has_builtin(__builtin_expect)
+#define expect(expr, cond) __builtin_expect(expr, cond)
+#else
+#define expect(expr, cond) (expr)
+#endif
+
+#if __has_builtin(__builtin_unreachable)
+#define unreachable() __builtin_unreachable()
+#else
+__attribute((noreturn)) static inline void unreachable(void)
+{
+}
+#endif
+
+#ifdef assert
+#undef assert
+#endif
+
 #ifdef NDEBUG
-#define assert(expr) (__builtin_expect(!!(expr), 1) ? (void)0 : __builtin_unreachable())
+#define assert(expr) (expect(!!(expr), 1) ? (void)0 : unreachable())
 #else
 #define assert(expr) ((expr) ? (void)0 : x__assert_fail(__FILE__, __LINE__, __func__, #expr))
 #endif
