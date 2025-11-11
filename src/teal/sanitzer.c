9af9a1d4b6fbe16c2b9eeca93e2d96b90c43d3a7
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-11-11 07:47:06 +0100

    Merge asan.c and lsan.c into one file

--- src/teal/sanitzer.c
+++ src/teal/sanitzer.c
@@ -1,3 +1,10 @@
+const char *__asan_default_options(void)  // NOLINT(bugprone-reserved-identifier)
+{
+    return "abort_on_error=1:"
+           "disable_coredump=0:"
+           "unmap_shadow_on_exit=1";
+}
+
 const char *__lsan_default_options(void)  // NOLINT(bugprone-reserved-identifier)
 {
     return "print_suppressions=0";
