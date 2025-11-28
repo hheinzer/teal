3cf69108a87ba8295af9e2ac2f4cf91f5270f457
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-11-28 21:22:27 +0100

    Add inlet test case

---
+++ run/inlet/mesh.geo
@@ -0,0 +1,30 @@
+SetFactory("OpenCASCADE");
+lc = 0.01;
+
+Point(1) = {0, 0, 0, lc};
+Point(2) = {8, 0, 0, lc};
+Point(3) = {8, 0.8, 0, lc};
+Point(4) = {0, 1.7, 0, lc};
+Point(5) = {2.1, 0.7, 0, lc};
+Point(6) = {4.2, 0.22, 0, lc};
+Point(7) = {6.7, 0.59, 0, lc};
+Point(8) = {5.9, 0.7, 0, lc};
+
+Line(1) = {1, 2};
+Line(2) = {2, 3};
+Line(3) = {3, 4};
+Line(4) = {4, 1};
+Line(5) = {5, 6};
+Line(6) = {6, 7};
+Line(7) = {7, 8};
+Line(8) = {8, 5};
+
+Curve Loop(1) = {1, 2, 3, 4};
+Curve Loop(2) = {5, 6, 7, 8};
+Plane Surface(1) = {1, 2};
+Extrude {0, 0, 1} { Surface{1}; Layers {1}; Recombine; }
+
+Physical Surface("inlet", 25) = {5};
+Physical Surface("outlet", 26) = {3};
+Physical Surface("wall", 27) = {2, 4, 9, 6, 7, 8};
+Physical Volume("domain", 28) = {1};
---
+++ run/inlet/run.c
@@ -0,0 +1,29 @@
+#include "equations.h"
+#include "euler.h"
+
+int main(int argc, char **argv)
+{
+    teal_initialize(&argc, &argv);
+
+    Mesh *mesh = mesh_read("run/inlet/mesh.msh");
+    mesh_generate(mesh);
+    mesh_summary(mesh);
+
+    Euler inlet = {.density = 1.4, .velocity = {.x = 3}, .pressure = 1};
+
+    Equations *eqns = euler_create(mesh);
+    equations_set_boundary_condition(eqns, "inlet", "supersonic inflow", &inlet, 0);
+    equations_set_boundary_condition(eqns, "outlet", "supersonic outflow", 0, 0);
+    equations_set_boundary_condition(eqns, "wall", "slipwall", 0, 0);
+    equations_set_initial_state(eqns, "domain", &inlet);
+    equations_summary(eqns);
+
+    Simulation *sim = simulation_create(eqns, argv[0]);
+    simulation_set_max_time(sim, 10);
+    simulation_set_out_time(sim, 0.1);
+    simulation_summary(sim);
+
+    simulation_run(sim);
+
+    teal_finalize();
+}
