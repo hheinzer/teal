27d7788d0fc834ecf9c72071739e166af96f2d3f
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-12-12 16:02:46 +0100

    Adjust Taylor-Green test case for performance testing

--- run/taylor_green_vortex/run.c
+++ run/taylor_green_vortex/run.c
@@ -1,7 +1,10 @@
 #define _GNU_SOURCE
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "navier_stokes.h"
+#include "teal/sync.h"
 
 scalar mach = 0.1, reynolds = 1600;
 Compute initial;
@@ -9,10 +12,11 @@ Compute initial;
 int main(int argc, char **argv)
 {
     teal_initialize(&argc, &argv);
+    long num = (argc > 1) ? strtol(argv[1], 0, 10) : 64;
 
     vector min_coord = {-M_PI, -M_PI, -M_PI};
     vector max_coord = {M_PI, M_PI, M_PI};
-    tuple num_cells = {64, 64, 64};
+    tuple num_cells = {num, num, num};
     bool periodic[] = {true, true, true};
     Mesh *mesh = mesh_create(min_coord, max_coord, num_cells, periodic);
     mesh_generate(mesh);
@@ -24,8 +28,11 @@ int main(int argc, char **argv)
     equations_set_property(eqns, "dynamic viscosity", 1 / reynolds);
     equations_summary(eqns);
 
-    Simulation *sim = simulation_create(eqns, argv[0]);
-    simulation_set_max_time(sim, 10);
+    char prefix[128];
+    sprintf(prefix, "%s_%04ld_%04ld", argv[0], num, sync.size);
+
+    Simulation *sim = simulation_create(eqns, prefix);
+    simulation_set_max_time(sim, 1);
     simulation_set_out_time(sim, 0.1);
     simulation_summary(sim);
 
@@ -41,7 +48,6 @@ void initial(void *variable_, const scalar *property, vector center, scalar time
     scalar pressure0 = 1 / (gamma * mach * mach);
     variable->velocity.x = sin(center.x) * cos(center.y) * cos(center.z);
     variable->velocity.y = -cos(center.x) * sin(center.y) * cos(center.z);
-    variable->velocity.z = 0;
     variable->pressure =
         pressure0 + ((cos(2 * center.x) + cos(2 * center.y)) * (cos(2 * center.z) + 2) / 16);
     variable->density = variable->pressure / pressure0;
