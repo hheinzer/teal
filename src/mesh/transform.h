4ed6ef8164c3ec5f0345cf27a67a83d0b3ec307a
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-12-24 19:10:27 +0100

    Remove old code

--- src/mesh/transform.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mesh.h"
-
-void transform_to_local(vector *res, const Basis *mat, const vector *vec);
-
-void transform_to_global(vector *res, const Basis *mat, const vector *vec);
--- src/simulation/advance.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "simulation.h"
-
-Advance euler, midpoint, heun, ralston, ssprk2, ssprk3, rk3, rk4, lserk;
-
-Advance implicit_euler;
