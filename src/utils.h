669e48022d24ccdc1cf47c6358f970294ac72459
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-12-29 03:18:41 +0100

    Add trap and breakpoint macros

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#define trap(rnk)                                 \
+    do {                                          \
+        if (sync.rank == (rnk)) __builtin_trap(); \
+    } while (0)
+
+#define breakpoint(rnk)                                       \
+    do                                                        \
+        if (sync.rank == (rnk)) __asm__ __volatile__("int3"); \
+    }                                                         \
+    while (0)
+
 #define sq(val) ((val) * (val))
 #define cb(val) ((val) * (val) * (val))
 
