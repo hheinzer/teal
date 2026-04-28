c7e4b629a65dc505c1127b43d9f620549c301006
Heinrich Heinzer <heinrich.heinzer@gmail.com>
2026-04-28 13:17:38 +0200

    Remove v5 code

--- src/equations/limiter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "equations.h"
-
-Limiter vanleer, vanalbada1, vanalbada2, mc, koren, minmod, superbee, venkatakrishnan;
-
-// Precompute per-cell epsilon^2 for the Venkatakrishnan limiter using cell volumes.
-scalar *venkatakrishnan_parameter(const Equations *eqns, scalar parameter);
--- src/simulation/gmres.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "simulation.h"
-
-void gmres(const Equations *eqns, const void *variable_, const void *derivative_,
-           const void *residual_, void *increment_, scalar time, scalar step, scalar norm,
-           scalar fd_scale, const NewtonKrylov *ctx);
